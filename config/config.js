e5398d7521d565802b5aab06c40f8aaaa982d3c0
FSOCIETYJS <jomicasierra@unac.edu.co>
2019-07-11 20:23:26 -0500

    campus

---
+++ config/config.js
@@ -0,0 +1,14 @@
+process.env.PORT = process.env.PORT || 3000;
+process.env.NODE_ENV = process.env.NODE_ENV || 'local';
+
+
+let urlDB
+if (process.env.NODE_ENV === 'local'){
+	urlDB = 'mongodb://localhost:27017/tienda';
+	
+}
+else {
+	urlDB = 'mongodb+srv://nodejstdea:nodejstdea@nodejstdea-4jn4i.mongodb.net/tienda?retryWrites=true'
+}
+
+process.env.URLDB = urlDB
